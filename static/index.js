f82b005f79b55c22a0dc2f2250e16e7010a61795
Migue <migue@tuta.io>
2021-07-20 15:37:43 -0400

    added deep leaning section and separation from web programmer

--- static/index.js
+++ static/index.js
@@ -1,10 +1,13 @@
 
 
 document.querySelector(".contact").addEventListener('click' , function () {
-    window.location.href = "mailto:miguel.minny@aleeas.com"
+    window.location.href = "mailto:contact@migue.pro"
 });
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
+
+// let controller1 = new ScrollMagic.Controller();
+// let timeline1 = new TimelineMax();
 
 timeline
     .to(".pasto", 10, { y: -100 })
@@ -14,14 +17,30 @@ timeline
     .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
     .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
 
+// timeline1
+//     .to("#deep-learning", 10, { top: "0%"})
+//     .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
+//     .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 };
+
 let scene = new ScrollMagic.Scene({
     triggerElement: "section",
+    // triggerElement: "nav",
     duration: "300%",
     triggerHook: 0,
 })
     .setTween(timeline)
+    // .setPin("nav")
     .setPin("section")
     .addTo(controller);
+
+// let dlscene = new ScrollMagic.Scene({
+//     triggerElement: "#deep-learning",
+//     duration: "300%",
+//     triggerHook: 0.5,
+// })
+//     .setTween(timeline1)
+//     .setPin("#deep-learning")
+//     .addTo(controller1);
 
 const parallax = document.querySelector(".parallax");
 window.addEventListener("scroll", function () {
