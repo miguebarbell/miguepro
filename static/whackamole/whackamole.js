46f339d795384756e9408e2c2450fe2716eab214
Miguel R <contact@migue.pro>
2021-09-12 10:37:44 -0400

    pause feature

--- static/whackamole/whackamole.js
+++ static/whackamole/whackamole.js
@@ -13,13 +13,12 @@ function createGrid() {
 
 createGrid()
 
-
-
 function start() {
-
     const square = document.querySelectorAll('.square')
-    const mole = document.querySelectorAll('.mole')
+    // const mole = document.querySelectorAll('.mole')
     const timeLeft = document.querySelector('#timeLeft')
+    let pause = false
+    // pauseGame()
     let score = document.querySelector('#result')
     let hitCell
 
@@ -43,7 +42,7 @@ function start() {
 
     square.forEach(id => {
         id.addEventListener('mouseup', () => {
-            if (id.id === hitCell) {
+            if ((id.id === hitCell) && (pause === false)) {
                 result += 1
                 score.textContent = result
             }
@@ -51,17 +50,19 @@ function start() {
     })
     let timeout = 1000
     let timerId = setInterval(countDown, 1000)
-    timerMole = setInterval(randomSquare, timeout)
-    function moveMole() {
-        let timerId = null
-        timerMole = setInterval(randomSquare, timeout)
-
-    }
+    let timerMole = setInterval(randomSquare, timeout)
+    // function moveMole() {
+    //     let timerId = null
+        // timerMole = setInterval(randomSquare, timeout)
+    //
+    // }
     function countDown() {
-        currentTime--
-        timeLeft.textContent = currentTime
-        makeItHarder(currentTime)
-        if (currentTime == 0) {
+        if (pause === false) {
+            currentTime--
+            timeLeft.textContent = currentTime
+            makeItHarder(currentTime)
+        }
+        if (currentTime === 0) {
             clearInterval(timerMole)
             clearInterval(timerId)
             const winningTextElement = document.querySelector('[data-winning-message-text]')
@@ -76,23 +77,44 @@ function start() {
         clearInterval(timerMole)
         timerMole = setInterval(randomSquare, timeout)
     }
+    // pause feature
+    const pauseButton = document.querySelector('#Pause')
+    pauseButton.onclick = pauseGame
+    document.addEventListener('keydown', function (e) {
+        if ((e.key === 'p') || (e.code === 'Space')) {
+            pauseGame()
+        }
+    })
+    function pauseGame() {
+        // pause the score and change the layout of the button
+        if (!pause) {
+            pause = true
+            clearInterval(timerMole)
+            pauseButton.innerHTML = 'Resume'
+        } else {
+            pause = false
+            timerMole = setInterval(randomSquare, timeout)
+            pauseButton.innerHTML = 'Pause'
+        }
+    }
     function makeItHarder (time) {
-        if (time == 50) {
+        if (time === 50) {
             timeout *= hardFactor
             resetInterval()
-        } else if (time == 40) {
+        } else if (time === 40) {
             timeout *= hardFactor
             resetInterval()
-        } else if (time == 30) {
+        } else if (time === 30) {
             timeout *= hardFactor
-        } else if (time == 20) {
+        } else if (time === 20) {
             timeout *= hardFactor
             resetInterval()
-        } else if (timeout == 10) {
+        } else if (time === 10) {
             timeout *= hardFactor
             resetInterval()
         }
     }
+    pauseGame()
 }
 start()
 function restart() {
@@ -101,17 +123,59 @@ function restart() {
 document.querySelector('#restartButton').onclick = restart
 function submitScore(result) {
     const name = prompt(`You scored ${result}, whats your name?`)
-    const url = 'https://miguepro.herokuapp.com/scores/swam'
-    // const url = 'http://127.0.0.1:8000/scores/swam'
-    const data = { name: name, score: result }
-    console.log(`name: ${name}: score: ${result}`)
+    if ((name !== '') && (name != null)) {
+        // const url = 'https://miguepro.herokuapp.com/scores/swam'
+        const url = 'http://127.0.0.1:8000/scores/swam'
+        const data = { name: name, score: result }
+        console.log(`name: ${name}: score: ${result}`)
+        fetch(url, {
+            headers: {
+              // 'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(data)}).then(response => {
+                console.log(response)
+        })
+} else {
+        console.log('Not entered any name')
+    }
+}
+function highscores() {
+    const url = 'https://miguepro.herokuapp.com/scores/wam'
+    // const url = 'http://127.0.0.1:8000/scores/wam'
+    const closeButton = document.querySelector('#closeHighScores')
     fetch(url, {
-        headers: {
-          // 'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(data)}).then(response => {
-            console.log(response)
-    })
+        // headers: {
+        //     'Content-Type' : 'application/json'
+        // },
+        method: "GET"
+
+    }).then(response => response.json())
+        .then(data => {
+            // select div in highscores
+            const highScoresTextElement = document.querySelector('[data-high-scores-text]')
+            const highScoresMessageElement = document.getElementById('highScores')
+            const div = document.createElement('div')
+            div.classList.add('hsTable')
+            div.innerHTML = '<h1>TOP 5 High Scores</h1>'
+            for (let i = 0; i < 5; i++) {
+                console.log(data[i])
+                let divText = document.createElement('p')
+                divText.innerHTML = `<h3>NAME: ${data[i].name}</h3> <h4>SCORE: ${data[i].score}</h4>`
+                div.appendChild(divText)
+                highScoresTextElement.appendChild(div)
+            }
+            highScoresMessageElement.classList.remove('hide')
+            highScoresMessageElement.classList.add('show')
+            closeButton.addEventListener('click', () => {
+                highScoresMessageElement.classList.add('hide')
+            })
+        })
+}
+highscores()
+function showHighScores() {
+    const highScoresMessageElement = document.getElementById('highScores')
+    highScoresMessageElement.classList.remove('hide')
+    highScoresMessageElement.classList.add('show')
 }
