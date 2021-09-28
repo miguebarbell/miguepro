b63552b87d1589b245428df7f3bf5869353271d1
Miguel R <contact@migue.pro>
2021-09-28 19:27:59 -0400

    refactores javascript and added transition to the burger

--- static/menu.js
+++ static/menu.js
@@ -2,20 +2,29 @@ let displaying = false
 function showMenu() {
     const menuDiv = document.querySelector('.menu')
     let ham = document.querySelector('.mini-ham-container')
-    if (displaying) {
+    function hideMenu() {
         menuDiv.style.display = 'none'
         displaying = false
         ham.style.setProperty('--ham1-rot', '0deg')
         ham.style.setProperty('--ham2-rot', '0deg')
         ham.style.setProperty('--ham1-h', '2rem')
         ham.style.setProperty('--ham2-h', '3rem')
-    } else {
+    }
+    function displayMenu() {
         menuDiv.style.display = 'flex'
         displaying = true
         ham.style.setProperty('--ham1-rot', '-45deg')
         ham.style.setProperty('--ham2-rot', '45deg')
         ham.style.setProperty('--ham1-h', '2.5rem')
         ham.style.setProperty('--ham2-h', '2.5rem')
+    }
+    menuDiv.addEventListener('click', () => {
+        hideMenu()
+    } )
+    if (displaying) {
+        hideMenu()
+    } else {
+        displayMenu()
     }
 }
 const nav = document.querySelector('nav')
