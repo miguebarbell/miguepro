70121486f6aeb6f0057c459756b3f1f10b55e918
Miguel R <contact@migue.pro>
2021-09-29 16:26:13 -0400

    excuses vanish when scroll down

--- static/excuses.js
+++ static/excuses.js
@@ -85,7 +85,11 @@ function load_excuse() {
 }
 window.addEventListener('scroll', () => {
 	let value = window.scrollY;
-	lazyElement.style.color = `rgb(${value/3},${value/5},${value/20})`
+	if (value === 0) {
+		lazyElement.style.color = 'black'
+	} else {
+		lazyElement.style.color = `rgba(${value/3},${value/5},${value/20}, ${value/(0.00001+value+value*2)})`
+	}
 	lazyElement.style.marginLeft = value + 'px'
 	lazyElement.style.marginTop = value/3 + 'px'
 	section.style.marginTop = -value/ratio + 'px';
