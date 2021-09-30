2bddff4bdd2a8dc5e4423984e2e2d4f08cbbfb37
Miguel R <contact@migue.pro>
2021-09-30 14:40:07 -0400

    form done

--- static/feedback.js
+++ static/feedback.js
@@ -3,7 +3,7 @@ const feedElement = document.createElement('span')
 feedElement.id = 'feed-element'
 feedElement.innerText = 'FEEDBACK'
 feedElement.setAttribute('title', 'Send your feedback or just say HI!.')
-const apiAdrr = 'https://www.migue.pro'
+// const apiAdrr = 'https://www.migue.pro'
 let displayingFeedbackElement = false
 
 // create the div for styling and the form
@@ -11,8 +11,9 @@ const formDiv = document.createElement('div')
 formDiv.innerText = 'Share your feedback please!'
 const form = document.createElement('form')
 // the name
-form.setAttribute('action', apiAdrr)
-form.setAttribute('method', 'POST')
+// form.setAttribute('action', apiAdrr)
+// form.setAttribute('method', 'POST')
+form.setAttribute('onsubmit', 'return false')
 
 formDiv.id = 'feedback-form'
 const name = document.createElement('input')
@@ -33,6 +34,18 @@ form.append(feedbackText)
 const button = document.createElement('input')
 button.setAttribute('type', 'submit')
 button.setAttribute('value', 'Submit')
+button.onclick = () => {
+    if (feedbackText.value !== '' && name.value !== '') {
+        // send the message
+        console.log('Feedback sent!. Thanks for reaching out')
+        displayingFeedbackElement = false
+        document.querySelector('body').removeChild(formDiv)
+        name.value = ''
+        feedbackText.value = ''
+        alert('Server is down, please reach at contact@migue.pro')
+
+    }
+}
 form.append(button)
 // say thanks after the button
 formDiv.append(form)
@@ -47,16 +60,3 @@ feedElement.onclick = () => {
 }
 bodyElement.appendChild(feedElement)
 
-
-function sendMessage () {
-    Email.send({
-        SecureToken: "cb4bfaa1-3a0a-4a49-9ed5-c29f8f2671bd",
-        To: 'feedback+contact=migue.pro@migue.pro',
-        From: "feedback@migue.pro",
-        Subject: "This is the subject",
-        Body: "And this is the body"
-    }).then(
-        // show something to the user about the feedback
-        message => alert(message)
-    );
-}
